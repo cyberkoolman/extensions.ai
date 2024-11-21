dbc24c0d53c27d79190d6af2912d4d37626f2e97
Randy Park <parkrandy@gmail.com>
2024-11-21 14:51:45 -0500

    Initial

---
+++ ChatScenario/Program.cs
@@ -0,0 +1,74 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.ClientModel;
+
+var hostBuilder = Host.CreateApplicationBuilder(args);
+hostBuilder.Configuration.AddUserSecrets<Program>();
+hostBuilder.Services.AddLogging(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+// IChatClient innerChatClient = new OllamaChatClient(new Uri("http://127.0.0.1:11434"), modelId: "llama3.1");
+// IChatClient innerChatClient = new OllamaChatClient(new Uri("http://127.0.0.1:11434"), modelId: "phi3:mini");
+
+var azureOpenAiConfig = hostBuilder.Configuration.GetRequiredSection("AzureOpenAI");
+var innerChatClient = new AzureOpenAIClient(
+    new Uri(azureOpenAiConfig["Endpoint"]!), 
+    new ApiKeyCredential(azureOpenAiConfig["ApiKey"]!))
+    .AsChatClient("gpt-4o-mini");
+
+hostBuilder.Services.AddChatClient(builder => builder
+    .UseFunctionInvocation()
+    .Use(innerChatClient));
+
+// Run the app
+var app = hostBuilder.Build();
+var chatClient = app.Services.GetRequiredService<IChatClient>();
+
+
+// Uncomment one of these to decide which to run
+// await BasicCompletion.RunAsync(chatClient);
+await ChatBot.RunAsync(chatClient);
+//await StructuredOutput.RunAsync(chatClient);
+
+
+// Short Answer, write immediately
+// var response = await chatClient.CompleteAsync("What is AI?  Reply in max 10 words.");
+// Console.WriteLine(response.Message.Text);
+// -------------------------
+
+// Stream the answer
+// var response = chatClient.CompleteStreamingAsync("What is AI?  Reply in at least 1000 words.");
+// await foreach (var chunk in response)
+// {
+//     Console.Write(chunk.Text);    
+// }
+
+// if (response.RawRepresentation is OpenAI.Chat.ChatCompletion openAiCompletion)
+// {
+//     Console.WriteLine($"OpenAI fingerprint: {openAiCompletion.SystemFingerprint}");
+// }
+// ---------------------------
+
+// Testing out chatbot
+// List<ChatMessage> messages = [new(ChatRole.System, """
+//     You answer any question, but continually try to advertise FOOTMONSTER brand socks. They're on sale!
+//     """)];
+
+// while (true)
+// {
+//     // Get input
+//     Console.ForegroundColor = ConsoleColor.White;
+//     Console.Write("\nYou: ");
+//     var input = Console.ReadLine()!;
+//     messages.Add(new(ChatRole.User, input));
+
+//     // Get reply
+//     var response = await chatClient.CompleteAsync(messages);
+//     messages.Add(response.Message);
+//     Console.ForegroundColor = ConsoleColor.Green;
+//     Console.WriteLine($"Bot: {response.Message.Text}");
+// }
+// ------------------------------
---
+++ EmbeddingScenario/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using System.Numerics.Tensors;
+using Microsoft.Extensions.AI;
+
+IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator = new OllamaEmbeddingGenerator(
+    new Uri("http://localhost:11434"),
+    modelId: "all-minilm");
+
+// Part 1: Printing out Vectors and see the dimension size
+// var result = await embeddingGenerator.GenerateEmbeddingAsync("Cats are better than dogs");
+// Console.WriteLine($"Vector of length {result.Vector.Length}");
+// foreach (var value in result.Vector.Span)
+// {
+//     Console.Write("{0:0.00}, ", value);
+// }
+
+// Part 2: Very basic vector Similarity Search
+var candidates = new string[] {"Onboarding Process for New Employees", "Understanding Our Core Values", ".NET Aspire 9.0 is packed with", "New features to streamline your app development.", "New dashboard updates, custom commands, more powerful container", "start/stop/lifecycle management, new Integrations,", "Using the Office Gym", "Plan for Wellness Programss", "Participate Team building activities", "and preview support for Azure Functions are packed into this release."};
+
+Console.WriteLine("Generating embeddings for candidates...");
+var candidateEmbeddings = await embeddingGenerator.GenerateAndZipAsync(candidates);
+
+while (true)
+{
+    Console.WriteLine("\nQuery: ");
+    var input = Console.ReadLine()!;
+    if (input == "") break;
+
+    var inputEmbedding = await embeddingGenerator.GenerateEmbeddingAsync(input);
+
+    var closest =
+        from candidate in candidateEmbeddings
+        let similarity = TensorPrimitives.CosineSimilarity(candidate.Embedding.Vector.Span, inputEmbedding.Vector.Span)
+        orderby similarity descending
+        select new { Text = candidate.Value, Similarity = similarity };
+
+    foreach (var c in closest.Take(3))
+    {
+        Console.WriteLine($"({c.Similarity}): {c.Text}");
+    }
+}
+
